1105c528916947329cb32680fa8225dc315b16a8
某室长 不知名的 <JABMDH@163.com>
2025-08-18 18:52:00 +0800

    更新说明

--- Src/main.cpp
+++ Src/main.cpp
@@ -1,8 +1,12 @@
 #include "CloudForgeAnalyzer.h"
 #include <QtWidgets/QApplication>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <cmath>
 
 int main(int argc, char *argv[])
 {
+
     QApplication app(argc, argv);
     CloudForgeAnalyzer win;
     win.show();
